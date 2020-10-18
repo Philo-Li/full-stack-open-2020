993b23e4e76ddd5730f85b22e7c63660b5035ea8
Leilei Lee <dreamlllp@163.com>
2020-10-18 11:15:59 +0800

    8.4 Books of an author

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -97,7 +97,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -106,7 +106,9 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, args) => {
+      return books.filter((book) => String(book.author) === String(args.author))
+    },
     allAuthors: (root) => authors,
   },
   Author: {
