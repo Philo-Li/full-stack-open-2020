1e4d0c857665c099936119e68da67ac27f570fec
Leilei Lee <dreamlllp@163.com>
2020-10-22 16:55:54 +0800

    8.19 Books by genre step1

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
+  const [select, setSelect] = useState(null)
   const { loading, error, data } = useQuery(ALL_BOOKS)
 
   if (!props.show) {
@@ -13,12 +14,29 @@ const Books = (props) => {
   if (error) return `Error! ${error.message}`
 
   const books = data.allBooks
+  let booksToShow = select ? books.filter((book) => book.genres.includes(select) === true) : books
+
   console.log(books)
+
+  let allGenres = []
+
+  books.map((book) => {
+    book.genres.map((genre) => {
+      if(!allGenres.includes(genre)){
+        allGenres = [...allGenres, genre]
+        console.log(genre, allGenres)
+      }
+    })
+  })
 
   return (
     <div>
       <h2>books</h2>
-
+      {select ? (
+        <p>in genre <b>{select}</b></p>
+      ):(
+        <p><b>all genres</b></p>
+      )}
       <table>
         <tbody>
           <tr>
@@ -30,7 +48,7 @@ const Books = (props) => {
               published
             </th>
           </tr>
-          {books.map(a =>
+          {booksToShow.map(a =>
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -39,6 +57,10 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      {allGenres.map(a => 
+        <button key={a} onClick={() => setSelect(a)}>{a}</button>
+      )}
+      <button key='allGenres' onClick={() => setSelect(null)}>all genres</button>
     </div>
   )
 }
--- part8/library-frontend/src/queries.js
+++ part8/library-frontend/src/queries.js
@@ -9,6 +9,7 @@ export const ALL_BOOKS = gql`
         born
       }
       published
+      genres
     }
   }
 `
